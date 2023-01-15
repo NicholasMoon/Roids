65afd82cff3857adc7a66a89e5d701a2398df2c4
NicholasMoon <moonmoon@seas.upenn.edu>
2023-01-15 18:56:29 -0500

    Initial commit

---
+++ Assets/Scripts/AsteroidScript.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AsteroidScript : MonoBehaviour
+{
+    public GameObject deathExplosion;
+    public int pointValue;
+    public AudioClip deathKnell;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        pointValue = 10;
+    }
+
+    public void Die()
+    {
+        AudioSource.PlayClipAtPoint(deathKnell,
+         gameObject.transform.position);
+        Instantiate(deathExplosion, gameObject.transform.position,
+        Quaternion.AngleAxis(-90, Vector3.right));
+        GameObject obj = GameObject.Find("GlobalObject");
+        Global g = obj.GetComponent<Global>();
+        g.score += pointValue;
+        Destroy(gameObject);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
